22937905858b0f47c2550bc8be0e0af11ef12dbd
Amirisettyrohith <amirisettyrohith@gmail.com>
2026-03-02 16:20:41 +0530

    Create UseCase5PalindromeCheckerApp.java

---
+++ App/src/UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        String input = "refer";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length(); i++) {
+            char poppedChar = stack.pop();
+            if (input.charAt(i) != poppedChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+    }
+}
