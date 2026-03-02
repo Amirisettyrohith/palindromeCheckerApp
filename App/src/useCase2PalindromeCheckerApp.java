643fee80481ead39f0e6c8fc7140e539cee5c939
Amirisettyrohith <amirisettyrohith@gmail.com>
2026-03-02 15:43:40 +0530

    Update useCase2PalindromeCheckerApp.java

--- App/src/useCase2PalindromeCheckerApp.java
+++ App/src/useCase2PalindromeCheckerApp.java
@@ -1,22 +1,13 @@
-import java.util.Scanner;
-
-public class useCase2PalindromeCheckerApp {
-    public static void main(String [] args){
-                Scanner sc = new Scanner(System.in);
-                System.out.print("Input text:");
-                String str = sc.nextLine();
-                String reversed = "";
-                for (int i = str.length() - 1; i >= 0; i--) {
-                    reversed += str.charAt(i);
-                }
-                if (str.equals(reversed)) {
-                    System.out.println("It is a palindrome?:true");
-                } else {
-                    System.out.println("It is not a palindrome?:false");
-                }
-                sc.close();
-
-
-
+public class UseCase2PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
