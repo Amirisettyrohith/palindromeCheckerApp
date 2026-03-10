70027789949c3a6434fc11d334e8c9f0ed64a619
Amirisettyrohith <amirisettyrohith@gmail.com>
2026-03-10 14:40:40 +0530

    [Amirisettyrohith] Added [usecase8]

---
+++ App/src/UseCase8PalindromeCheckerApp.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class UseCase8PalindromeCheckerApp {
+
+    Node head;
+    public void insert(char data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+    public Node reverse(Node node) {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    public boolean isPalindrome() {
+
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+
+        Node tempSecond = secondHalf;
+
+        while (tempSecond != null) {
+            if (firstHalf.data != tempSecond.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            tempSecond = tempSecond.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        UseCase8PalindromeCheckerApp list = new UseCase8PalindromeCheckerApp();
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+
+        for (int i = 0; i < input.length(); i++) {
+            list.insert(input.charAt(i));
+        }
+
+        if (list.isPalindrome()) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
