141f1b9819e08628929e72510aa8ed9faca0c230
Amirisettyrohith <amirisettyrohith@gmail.com>
2026-03-10 15:32:02 +0530

    [Amirisettyrohith] added [uc8]

--- App/src/UseCase8PalindromeCheckerApp.java
+++ App/src/UseCase8PalindromeCheckerApp.java
@@ -53,7 +53,7 @@ public class UseCase8PalindromeCheckerApp {
         Node fast = head;
 
 
-        while (fast != null && fast.next != null) {
+         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
